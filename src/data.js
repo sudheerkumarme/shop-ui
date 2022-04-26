53bd3f810167cd0837e55c55f69407bcaeb2ea8a
Sudheer Kumar <masinasudheerkumar@gmail.com>
2022-04-26 18:20:39 +0000

    Create announcement, slider, categories, Products and Footer

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://objectstorage.ap-hyderabad-1.oraclecloud.com/p/3oLUc8X5RKJOhejZ4W6hBQjOZ92j0Mupx8LHZiempu2T0ywC4pzhJHVlTOU-1rQI/n/axlzkw7dix0m/b/dev/o/slideImage-removebg.png",
+        title: "SUMMER SALE",
+        desc: "Don't compromise on style! Get flat 30% off on new arrivals.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://objectstorage.ap-hyderabad-1.oraclecloud.com/p/3oLUc8X5RKJOhejZ4W6hBQjOZ92j0Mupx8LHZiempu2T0ywC4pzhJHVlTOU-1rQI/n/axlzkw7dix0m/b/dev/o/slideImage-removebg.png",
+        title: "AUTUMN SALE",
+        desc: "Don't compromise on style! Get flat 30% off on new arrivals.",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://objectstorage.ap-hyderabad-1.oraclecloud.com/p/3oLUc8X5RKJOhejZ4W6hBQjOZ92j0Mupx8LHZiempu2T0ywC4pzhJHVlTOU-1rQI/n/axlzkw7dix0m/b/dev/o/slideImage-removebg.png",
+        title: "WINTER SALE",
+        desc: "Don't compromise on style! Get flat 30% off on new arrivals.",
+        bg: "fbf0f4"
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "SHIRT STYLE!"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "LOUNGWEAR LOVE"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        title: "LIGHT JACKETS"
+    }
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg"
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80"
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1497339100210-9e87df79c218"
+    },
+    {
+        id: 4,
+        img: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8"
+    },
+    {
+        id: 5,
+        img: "https://images.unsplash.com/photo-1543076447-215ad9ba6923"
+    },
+    {
+        id: 6,
+        img: "https://images.unsplash.com/photo-1564584217132-2271feaeb3c5"
+    },
+    {
+        id: 7,
+        img: "https://images.unsplash.com/photo-1544441893-675973e31985"
+    },
+    {
+        id: 8,
+        img: "https://images.unsplash.com/photo-1603252110971-b8a57087be18"
+    }
+];
