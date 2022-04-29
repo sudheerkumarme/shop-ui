072126bc2414093c3b29f1da8f891a3923fe8b39
Sudheer Kumar <masinasudheerkumar@gmail.com>
2022-04-29 14:15:30 +0000

    Make the pages responsive to mobile screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import { Search, ShoppingCartOutlined } from '@mui/icons-material'
+import { Login, Search, ShoppingCartOutlined } from '@mui/icons-material'
 import { Badge } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `
 
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `
 
 const Left = styled.div`
@@ -24,16 +27,19 @@ const Center = styled.div`
 `
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px"})}
 `
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ justifyContent: "center"})}
 `
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `
 const SearchContainer = styled.div`
   border: 1px solid lightgray;
@@ -44,6 +50,22 @@ const SearchContainer = styled.div`
 `
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
+`
+const RegisterButton = styled.button`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  padding: 10px 10px;
+  background-color: transparent;
+  border: 1px solid lightgray;
+  ${mobile({ display: "none" })}
+`
+const LoginButton = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ display: `${props => props.type === "register" && "none"}`, fontSize: "14px"})}
 `
 const MenuItem = styled.div`
   font-size: 14px;
@@ -58,7 +80,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input/>
+            <Input placeholder='Search'/>
             <Search style={{color:"gray", fontSize:"16px"}}/>
           </SearchContainer>
         </Left>
@@ -66,12 +88,12 @@ const Navbar = () => {
           <Logo>techreside.</Logo>
         </Center>
         <Right>
-          <MenuItem>REGISTER</MenuItem>
-          <MenuItem>LOGIN</MenuItem>
+          <RegisterButton>CREATE AN ACCOUNT</RegisterButton>
+          <LoginButton><Login/></LoginButton>
           <MenuItem>
-          <Badge badgeContent={4} color="primary">
-            <ShoppingCartOutlined/>
-          </Badge>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined/>
+            </Badge>
           </MenuItem>
         </Right>
       </Wrapper>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,6 @@
 import { Send } from "@mui/icons-material";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60vh;
@@ -13,13 +14,14 @@ const Container = styled.div`
 const Title = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
-
+  ${mobile({ fontSize: "54px" })}
 `;
 
 const Desc = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ fontSize: "16px", textAlign: "center" })}
 `;
 
 const InputContainer = styled.div`
@@ -29,6 +31,7 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${mobile({ width: "80%" })}
 `;
 
 const Input = styled.input`
